9d359d65a564b3fd623d72a03329beb08873324b
Jucong He <hejucong1995@gmail.com>
2017-02-08 22:17:49 -0500

    finsihed

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main(){
 		if (cmd[0] == '\n') continue;
 		shell->parse_command(cmd, cmdTokens);
 		if (shell->isQuit(*cmdTokens)){
+			// std::cout << "Exiting " << '\n';
 			exit(0);
 		}
 		else{
--- src/tsh.cpp
+++ src/tsh.cpp
@@ -4,6 +4,12 @@
 #include <string.h>
 using namespace std;
 
+void cleanBuffer(char **buffer) {
+  // std::cout << "Cleaning buffer" << '\n';
+  for (unsigned int i = 0; i < 25; i++) {
+    buffer[i] = NULL;
+  }
+}
 void simple_shell::parse_command(char* cmd, char** cmdTokens) {
   // TODO: tokenize the command string into arguments
   // printf("Command I enter is %s\n",cmd);
@@ -11,7 +17,7 @@ void simple_shell::parse_command(char* cmd, char** cmdTokens) {
   string token;
   int index = 0;
   while(iss>>skipws>>token) {
-    cmdTokens[index] = new char[token.length() + 1];
+    cmdTokens[index] = new char[token.length()];
     strcpy(cmdTokens[index], token.c_str());
     index += 1;
   }
@@ -22,16 +28,22 @@ void simple_shell::exec_command(char **argv)
 {
   // TODO: fork a child process to execute the command.
   // parent process should wait for the child process to complete and reap it
-  while(*argv != NULL) {
-    printf("%s\n",*argv);
-    *argv = NULL;
-    *argv++;
+  if (fork() == 0) {
+    if (execvp(*argv, argv) == -1) {
+      // printf("can't execute command %s\n",*argv);
+      cleanBuffer(argv);
+    } else {
+      execvp(*argv, argv);
+    }
+  } else {
+    wait(NULL);
+    cleanBuffer(argv);
   }
 }
 
 bool simple_shell::isQuit(char *cmd){
   // TODO: check for the command "quit" that terminates the shell
-  // std::cout << cmd << '\n';
+  // std::cout << "Checking quit" << '\n';
   if (strcmp(cmd,"quit") == 0) {
     // std::cout << "Quit got!" << '\n';
     return true;
